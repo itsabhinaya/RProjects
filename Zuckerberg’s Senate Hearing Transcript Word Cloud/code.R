9a656017cc773808b3b4232d965371ae9b22bda1
itsabhinaya <theabhinaya@gmail.com>
2018-04-10 22:14:45 -0500

    "team" lines

--- Zuckerberg’s Senate Hearing Transcript Word Cloud/code.R
+++ Zuckerberg’s Senate Hearing Transcript Word Cloud/code.R
@@ -3,11 +3,14 @@ library(wordcloud)
 library(RColorBrewer)
 
 lines <- readLines("fb.txt")
+fb_script<-Corpus(VectorSource(lines))
 fb_script_modi<-tm_map(fb_script,stripWhitespace)
 fb_script_modi <- tm_map(fb_script_modi, removePunctuation)
 fb_script_modi <- tm_map(fb_script_modi, removeWords, stopwords('english'))
 
-png("FB_wordcloud.png", width=12, height=12, units="in", res=300)
+png("FB_wordcloud_1.png", width=12, height=12, units="in", res=300)
 wordcloud(fb_script_modi, random.order=F, min.freq=1, colors=brewer.pal(8, "Dark2"), vfont=c("sans serif","plain"))
 dev.off()
 
+team_lines = grep("team", lines, value = TRUE)
+write(team_lines, "team_lines.txt")
